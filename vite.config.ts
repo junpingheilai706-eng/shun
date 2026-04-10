983f19f94c16e35a337d7e80cc828e7935cf6f08
shunshun0709 <junpingheilai706@gmail.com>
2026-04-10 01:55:51 +0000

    shun

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' //
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/shun/', // 
   plugins: [
     react(),
-    tailwindcss(), //
+    tailwindcss(), // 
   ],
 })
